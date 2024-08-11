a1f04ad1e14dab4f6e8dd68dbca7b79178fad794
Asghar Ali <asgharali.224415@gmail.com>
2024-08-11 05:12:11 +0500

    upda

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
+import { postcss } from "tailwindcss"
+
 export default {
   plugins: {
+    'postcss-import': {},
     tailwindcss: {},
     autoprefixer: {},
   },
+  from: undefined, // Explicitly setting this to avoid the warning
 }
